d8a19dd1f05e79c7f58649d54c52b4a69256e593
belarr0 <belarr00ss@gmail.com>
2024-10-16 17:26:08 +0300

    pull from git

--- test.py
+++ test.py
@@ -61,39 +61,57 @@ from time import time
 #     asyncio.run(main())
 
 
-def blocks(n):
-    counter = n
-    start = time()
-    while counter > 0:
-        counter -= 1
-    return time() - start
+# def blocks(n):
+#     counter = n
+#     start = time()
+#     while counter > 0:
+#         counter -= 1
+#     return time() - start
+#
+#
+# async def monitoring():
+#     while True:
+#         await asyncio.sleep(2)
+#         print(f'Monitoring {time()}')
+#
+#
+# async def run_blocking_tasks(executor, n):
+#     loop = asyncio.get_event_loop()
+#     print('waiting for executor tasks')
+#     result = await loop.run_in_executor(executor, blocks, n)
+#     return result
+#
+#
+# async def main():
+#     asyncio.create_task(monitoring())
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+#         futures = [run_blocking_tasks(executor, n) for n in [50_000_000, 60_000_000, 70_000_000]]
+#         results = await asyncio.gather(*futures)
+#         return results
+#
+#
+# if __name__ == '__main__':
+#     result = asyncio.run(main())
+#     for r in result:
+#         print(r)
 
 
-async def monitoring():
-    while True:
-        await asyncio.sleep(2)
-        print(f'Monitoring {time()}')
 
-
-async def run_blocking_tasks(executor, n):
-    loop = asyncio.get_event_loop()
-    print('waiting for executor tasks')
-    result = await loop.run_in_executor(executor, blocks, n)
-    return result
+async def baz() -> str:
+    print('Before Sleep')
+    await asyncio.sleep(1)
+    print('After Sleep')
+    return 'Hello world'
 
 
 async def main():
-    asyncio.create_task(monitoring())
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        futures = [run_blocking_tasks(executor, n) for n in [50_000_000, 60_000_000, 70_000_000]]
-        results = await asyncio.gather(*futures)
-        return results
+    r = baz()
+    print(r)
+    result = await r
+    print(result)
 
 
 if __name__ == '__main__':
-    result = asyncio.run(main())
-    for r in result:
-        print(r)
-
+    asyncio.run(main())
 
 
